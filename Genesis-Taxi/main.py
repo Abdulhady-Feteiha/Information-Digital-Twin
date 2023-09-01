782fb1945e0971829c84e1b6c75d59ff6038274c
Amr abdalbadee <amrabdalbadee@gmail.com>
2023-09-01 22:04:25 +0300

    added original environment

--- Genesis-Taxi/main.py
+++ Genesis-Taxi/main.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
         q_table,episodes_num_steps,epsiodes_mean_reward,epsiodes_cumulative_reward,episodes_entropy,episodes_penalty,episodes_info_gain = agent.train()
         save_training_progress(q_table,episodes_num_steps,epsiodes_mean_reward,epsiodes_cumulative_reward,episodes_entropy,episodes_penalty,episodes_info_gain)
     else:
-        # agent.display()
         agent.test()
+    if config.display_flag:
+        agent.display()
     
     # evaluate()
